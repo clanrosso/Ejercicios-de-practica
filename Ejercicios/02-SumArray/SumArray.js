d59096779859bf2ba379759cb0e7c701fcced9b5
clanrosso <claudiorossocba@hotmail.com.ar>
2022-09-28 18:04:09 -0300

    Ejercicio 2 listo

--- Ejercicios/02-SumArray/SumArray.js
+++ Ejercicios/02-SumArray/SumArray.js
@@ -1,5 +1,25 @@
 function SumArray (arr, n) {
   // Your code here:
+  
+  /* for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if(arr[i] + arr[j] === n) return true
+    }
+  }
+  return false */
+​
+let start = 0;
+let end = arr.length - 1;
+​
+while(start < end){
+  let suma = arr[start] + arr[end];
+​
+  if(suma > n) end -= 1;
+  else if(suma < n) start += 1;
+  else return true
+}
+return false
+
 
 }
 
