8c329aaa353b2d4f4b722b48c964802f30f0a70c
clanrosso <claudiorossocba@hotmail.com.ar>
2022-09-28 18:02:33 -0300

    Ejercicio 1 listo

--- Ejercicios/01-FindNeedle/FindNeedle.js
+++ Ejercicios/01-FindNeedle/FindNeedle.js
@@ -1,14 +1,6 @@
-function FindNeedle (haystack, needle) {
+function FindNeedle(haystack, needle) {
   // Your code here:
-  //recorrer el haystack letra por letra
-  //por cada letra del haystack, ver si coincide con la primera letra del needle
-    //si coinciden, recorremos el needle
-    //si llegamos al final, es porque el needle está contenido completamente y por tanto devolvemos el índice donde inician las coincidencias
 
-    //si no coinciden, deescartamos y pasamos a la siguiente
-    //si nunca la encontramos, retorno -1
-    //si length needle > length haystack retorno -1
-  
   /* for (let i = 0; i < haystack.length; i++) {   
     for (let j = 0; j < needle.length; j++) {      
       if(needle[j] !== haystack[i + j]) break;
@@ -19,31 +11,17 @@ function FindNeedle (haystack, needle) {
 
   for (let i = 0; i < haystack.length; i++) {
     let sliced = haystack.slice(i, i + needle.length);
-    if(needle === sliced) return i
+    if (needle === sliced) return i;
   }
-  return -1
-  
+  return -1;
 }
 
 //Complejidad temporal ALT I
 
 // haystack.length = n ; needle.length = m --> O(n*m)
 
-//Peor casos:
-
-//haystack = 'aaaaaaaaaaaab'
-//needle = 'b'
-
-
 //complejidad temporal ALT II
 
 // haystack.length = n ; needle.length = m --> O(n*m)
 
-//.slice() --> O(n)
-
-module.exports = FindNeedle
-
-
-console.log(FindNeedle('react-redux', 'redux'))//6
-console.log(FindNeedle('pinky', 'puntual'))//-1
-console.log(FindNeedle('rereredux', 'reredux'))//2
+module.exports = FindNeedle;
