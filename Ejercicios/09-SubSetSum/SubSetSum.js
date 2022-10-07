f563b4b92a52c9672e2ad42c9f8f2ddd615d60e3
clanrosso <claudiorossocba@hotmail.com.ar>
2022-10-07 01:56:25 -0300

    Ejercicio 9 listo

--- Ejercicios/09-SubSetSum/SubSetSum.js
+++ Ejercicios/09-SubSetSum/SubSetSum.js
@@ -1,6 +1,62 @@
 function subsetSum (nums, n) {
   // Your code here:
-
+​
+/*   let suma = nums[0];
+  if(suma === target) return true
+  for (let i = 1; i < nums.length; i++) {
+    if(nums[i] === target) return true
+​
+    if(suma + nums[i] <= target){
+      suma += nums[i]
+    }
+  }
+  if(suma === target) return true
+  else return false
+ */
+  /* let sum = [];
+  for (const num of nums) {
+    console.log(num);
+​
+    if(num === target) return true
+​
+    if(num < target) sum.push(num)
+    sum
+  }
+  var total = sum.reduce((n,m) => n + m)
+  if(total === target) return true
+  else return false */
+​
+  /* let result = 0;
+  nums.map((item, i) => {
+    let sum = item;
+    nums.map((item2, i2) => {
+      if (i !== i2) {
+        sum += item2;
+        if (sum>n) {
+          sum -= item2;
+        }
+      }
+    })    
+    if (sum===n) {
+      result = sum;
+    }
+  })
+  return result===n; */
+​
+  
+  //Nicolas
+​
+  if (nums.includes(n)) return true
+  if (nums.length <= 1) return false
+​
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < n) {
+      const newNums = [...nums]
+      newNums.splice(i, 1)
+      const newTarget = n - nums[i]
+      if (subsetSum(newNums, newTarget)) return true
+    }
+  }
+  return false
 }
-
 module.exports = subsetSum
