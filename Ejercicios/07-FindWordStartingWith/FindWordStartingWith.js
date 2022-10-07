092b04eadcce3f3e9b22c2ae81c30226b18fda01
clanrosso <claudiorossocba@hotmail.com.ar>
2022-10-07 01:52:59 -0300

    Ejercicio 7 listo

--- Ejercicios/07-FindWordStartingWith/FindWordStartingWith.js
+++ Ejercicios/07-FindWordStartingWith/FindWordStartingWith.js
@@ -1,6 +1,57 @@
 function FindWordStartingWith (book, query) {
   // Your code here:
-
+/*   const txt = book.text.toLowerCase();
+  query = query.toLowerCase();
+  console.log(query);
+  let result = [];
+​
+  if(txt.slice(0,query.length) === query){
+    result.push(0)
+  }
+  for (let i = 1; i < txt.length; i++) {
+    const sliced = txt.slice(i, i + query.length)
+    if(txt[i - 1] === ' ' && sliced === query){
+      result.push(i)
+    }
+  return result
+ */
+​
+/*   const word = ' ' + query.toLowerCase()
+  const coincidences = []
+  for (let i = 0; i < book.text.length; i++) {
+    if (book.text[i] === word[0]){
+      const wordBook = book.text.slice(i, i + word.length)
+      if (wordBook === word) coincidences.push(i + 1)
+    }
+  }
+  return coincidences
+ */
+​
+  /* const txt = book.text.toLowerCase();
+  const id = book.id;
+  let result = [];
+​
+  let listWord = txt.split(' ');
+  let punter = 1;
+  listWord.map((item,i) => {    
+    if (id>=punter) {
+      if (' '+item.indexOf(' '+query)) {
+        result.push(punter);
+      }
+      punter += item.length;
+    }
+  })
+  return result 
+  */
+  
+  const words = book.text.toLowerCase().split(' ') 
+  const indexes = []
+  let i = 0
+  for (const word of words) {
+    if (word.startsWith(query.toLowerCase())) indexes.push(i)
+    i += word.length + 1
+  }
+  return indexes
 }
-
+​
 module.exports = FindWordStartingWith
